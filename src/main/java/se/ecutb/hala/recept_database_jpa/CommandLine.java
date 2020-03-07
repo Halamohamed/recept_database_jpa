8ab87f06150d433fd7f8418314b546282d03c8f0
Hala Mohammed <halaalimohammed@gmail.com>
2020-03-07 16:48:31 +0100

    added CommandLine

--- src/main/java/se/ecutb/hala/recept_database_jpa/CommandLine.java
+++ src/main/java/se/ecutb/hala/recept_database_jpa/CommandLine.java
@@ -5,8 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import se.ecutb.hala.recept_database_jpa.entity.*;
-import se.ecutb.hala.recept_database_jpa.service.CreateIngredientService;
-import se.ecutb.hala.recept_database_jpa.service.CreateRecipeService;
+import se.ecutb.hala.recept_database_jpa.service.*;
 
 import javax.persistence.EntityManager;
 import java.util.ArrayList;
@@ -17,44 +16,60 @@ public class CommandLine implements CommandLineRunner {
 
     private CreateIngredientService ingredientService;
     private CreateRecipeService recipeService;
+    private RecipeInstructionService instructionService;
+    private RecipeIngredientService recipeIngredientService;
+    private RecipeCategoryService categoryService;
+
+    List<RecipeIngredient> recipeIngredients = new ArrayList<>();
+    List<Recipe> mainRecipes = new ArrayList<>();
+    List<RecipeCategory> categories = new ArrayList<>();
 
 
     @Autowired
-    public CommandLine(CreateIngredientService ingredientService, CreateRecipeService recipeService) {
+    public CommandLine(CreateIngredientService ingredientService, CreateRecipeService recipeService, RecipeInstructionService instructionService, RecipeIngredientService recipeIngredientService, RecipeCategoryService categoryService) {
         this.ingredientService = ingredientService;
         this.recipeService = recipeService;
+        this.instructionService = instructionService;
+        this.recipeIngredientService = recipeIngredientService;
+        this.categoryService = categoryService;
     }
 
     @Override
-
     public void run(String... args) throws Exception {
+        addIngredient();
+        addRecipeIngredient();
+        addRecipeCategory();
+        addInstruction();
+        addRecipe();
 
-        List<RecipeIngredient> recipeIngredients = new ArrayList<>();
-        List<Recipe> mainRecipes = new ArrayList<>();
-        List<RecipeCategory> categories = new ArrayList<>();
+    }
 
+    public void addRecipeIngredient(){
+        Ingredient ingredient1 = new Ingredient("chicken");
+        RecipeIngredient recipeIngredient1 = new RecipeIngredient(ingredient1,500, Measurement.G,null);
+        recipeIngredients.add(recipeIngredient1);
+    }
+    public void addInstruction(){
+        String instruction = "test instruction";
+        instructionService.createAndSave(instruction);
+    }
+
+    public void addRecipe(){
+        String recipeName = "Fried chicken";
         RecipeInstruction instruction = new RecipeInstruction("Cut the whole chickens into 4 breasts, 4 thighs, 4 legs and 4 wings and set aside. Preheat your oil, " +
                 "in either a heavy pan on the stove or a deep-fryer, to 325 degrees F. In a large bowl, combine the flour, salt, black pepper, garlic powder," +
                 " onion powder and cayenne pepper until thoroughly mixed");
 
-
-        RecipeCategory mainDishes = new RecipeCategory("Main Dishes",mainRecipes);
-
-
-        Ingredient ingredient1 = new Ingredient("chicken");
-
-
-
-        RecipeIngredient recipeIngredient1 = new RecipeIngredient(ingredient1,500, Measurement.G,null);
-
-
-        recipeIngredients.add(recipeIngredient1);
-
-
-        //System.out.println(ingredientService.createAndSave("chicken"));
-
-        recipeService.createAndSave("Fried chicken",recipeIngredients,instruction,categories);
-
+        recipeService.createAndSave(recipeName,recipeIngredients,instruction,categories);
+    }
+    public void addIngredient(){
+        String ingredientName = "onion";
+        System.out.println(ingredientService.createAndSave(ingredientName));
+    }
+    public void addRecipeCategory(){
+        String category = "Main Dishes";
+        RecipeCategory mainDishes = new RecipeCategory(category,mainRecipes);
+        categoryService.createAndSave(category,mainRecipes);
 
     }
 }
